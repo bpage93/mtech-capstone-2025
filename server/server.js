5bc43a3da2d5a67e8eca5192286bfbd664168480
bpage93 <bradpage93@gmail.com>
2025-07-11 10:04:10 -0600

    added morgan and winston

--- server/server.js
+++ server/server.js
@@ -1,18 +1,37 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+const winston = require("winston");
 
 const authRoutes = require("./routes/auth");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(
+    morgan("combined", {
+        stream: {
+            write: (message) => logger.http(message.trim()),
+        },
+    })
+);
+
+const logger = winston.createLogger({
+    level: "http", // Default is info
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: "logfile.log" }),
+    ],
+});
 
 app.use(cors());
 app.use(express.json()); // Required for req.body
 app.use("/api/auth", authRoutes);
 
 app.get("/api", (req, res) => {
+    logger.http("api endpoint requested");
+
     res.json({ message: "Hello from server!" });
 });
 
