7e7ad3bae6504a7855dbe7ea775c6f2b5cd434fb
Brett Boettcher <hgdxg@BrettsRTX>
2025-07-20 22:36:31 -0600

    added password hashing on account creation

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const { pool, query } = require("../database/postgresQuery");
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const router = express.Router();
 
 router.get("/get", async (req, res) => {
 	try {
-		const results = await query('SELECT * FROM "user"');
+        const results = await query(`
+            SELECT
+                usr.role,
+                usr.email,
+                usr.firstname,
+                usr.lastname,
+                usr.telephone,
+                usr.username,
+                address.street,
+                address.city,
+                address.state,
+                address.zip
+            FROM "user" usr JOIN address ON usr.id = address.user_id;
+        `);
 		res.status(200).json(results.rows);
 	} catch (error) {
 		res.status(500).json({ error: error.message });
@@ -17,13 +32,14 @@ router.post("/create", async (req, res) => {
     const user = req.body.user;
     
     const client = await pool.connect();
-	try {
+    try {
         await client.query('BEGIN');
+        const hashedPassword = await bcrypt.hash(user.password, saltRounds);
         const userResult = await client.query(`
             INSERT INTO "user" (role, email, firstname, lastname, telephone, username, password) VALUES
                 ($1, $2, $3, $4, $5, $6, $7)
             RETURNING id
-        `, ["student", user.email, user.firstname, user.lastname, user.telephone, user.username, user.password]);
+        `, ["student", user.email, user.firstname, user.lastname, user.telephone, user.username, hashedPassword]);
 
         const userId = userResult.rows[0].id;
 
