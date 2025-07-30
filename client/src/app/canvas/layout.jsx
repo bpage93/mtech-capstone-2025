d590f1f30b1a793ec88c78c13a3f52c6c3506ea7
bpage93 <bradpage93@gmail.com>
2025-07-30 16:50:43 -0600

    added new style/ui to setting pages

--- client/src/app/canvas/layout.jsx
+++ client/src/app/canvas/layout.jsx
@@ -7,38 +7,38 @@ import Loading from "./loading";
 import { Suspense } from "react";
 
 export default function Layout({ children }) {
-	return (
-		<RouteGuard>
-			<TitleWrapper>
-				<MainLayout>
-					<Suspense fallback={<Loading />}>{children}</Suspense>
-				</MainLayout>
-			</TitleWrapper>
-		</RouteGuard>
-	);
+    return (
+        <RouteGuard>
+            <TitleWrapper>
+                <MainLayout>
+                    <Suspense fallback={<Loading />}>{children}</Suspense>
+                </MainLayout>
+            </TitleWrapper>
+        </RouteGuard>
+    );
 }
 
 function MainLayout({ children }) {
-	const { title } = useTitleContext();
+    const { title } = useTitleContext();
 
-	return (
-		<div className="w-full max-w-screen max-h-screen min-h-dvh flex p-3 bg-[#1B1340] gap-3 relative">
-			<Sidebar />
+    return (
+        <div className="w-full max-w-screen max-h-screen min-h-dvh flex p-3 bg-[#1B1340] gap-3 relative">
+            <Sidebar />
 
-			<div className="flex flex-col gap-4 w-full overflow-auto">
-				<div className="flex flex-col gap-y-3 rounded-md h-full">
-					<div className="bg-[#120C2A] rounded-xl px-7 py-5 h-full flex flex-col gap-y-8">
-						<div className="">
-							<TitleDisplay title={title} />
-						</div>
-						{children}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+            <div className="flex flex-col gap-4 w-full overflow-auto">
+                <div className="flex flex-col gap-y-3 rounded-md h-full">
+                    <div className="bg-[#120C2A] rounded-xl px-7 py-5 h-full flex flex-col gap-y-8">
+                        <div className="">
+                            <TitleDisplay title={title} />
+                        </div>
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 function TitleDisplay({ title }) {
-	return <h2 className="flex items-center text-2xl font-bold">{title}</h2>;
+    return <h2 className="flex items-center text-2xl font-bold">{title}</h2>;
 }
