a70e9f0f014a097ae44650c36fb5cfa654782887
Brett Boettcher <hgdxg@BrettsRTX>
2025-07-30 19:59:42 -0600

    added main loading page to the profile page and added logout button to the profile page

--- client/src/app/canvas/profile/page.js
+++ client/src/app/canvas/profile/page.js
@@ -4,11 +4,13 @@ import { useState, useEffect } from "react";
 import { useTitleContext } from "../contexts/TitleContext";
 import { useRouter } from "next/navigation";
 import Loading from "@/app/canvas/loading";
-import { setLazyProp } from "next/dist/server/api-utils";
+import MainLoading from "@/app/canvas/loading";
+import { Mic } from "@mui/icons-material";
 
 export default function SettingsPage() {
 	const { updateTitle } = useTitleContext();
 	const [loading, setLoading] = useState(true);
+	const [mainLoading, setMainLoading] = useState(true);
 	const [token, setToken] = useState("");
 	const [user, setUser] = useState();
 	const [editingField, setEditingField] = useState();
@@ -16,6 +18,7 @@ export default function SettingsPage() {
 	const router = useRouter();
 
 	useEffect(() => {
+		setMainLoading(false);
 		updateTitle("Profile");
 		const storedToken = localStorage.getItem("jwtToken");
 		setToken(storedToken);
@@ -72,41 +75,57 @@ export default function SettingsPage() {
 		setLoading(false);
 	}
 
-	return (
-		<div className="bg-[#160f33] h-full w-full rounded-lg overflow-auto">
-			{loading ? (
-				<Loading />
-			) : (
-				<div className="min-w-100 w-full shrink xl:min-w-full overflow-x-auto p-4 flex flex-col gap-y-4">
-					<div className="grid grid-cols-1 lg:grid-cols-2 gap-y-4 gap-x-2">
-						{user &&
-							Object.entries(user).map(([field, value]) => {
-								return (
-									<div key={field} className="flex justify-between items-center min-w-fit w-full shadow/30 bg-indigo-950/40 rounded-lg hover:bg-indigo-950 border-b border-b-white/5 p-3">
-										<div className="flex flex-col text-lg h-full shrink">
-											<h6 className="text-gray-400 truncate">{field}</h6>
-											{field === editingField ? <input id="editing-value" value={editingFieldValue} onChange={(e) => setEditingFieldValue(e.target.value)} onBlur={() => setEditingField(null)} /> : <span className="truncate">{value}</span>}
-										</div>
+	function logout() {
+		setMainLoading(true);
+		localStorage.setItem("jwtToken", "");
+		router.push("/");
+	}
 
-										<button
-											className="justify-center items-center flex hover:cursor-pointer bg-indigo-700 rounded-md p-1 w-10 h-10 shrink-0"
-											aria-label="Edit this value"
-											onClick={() => {
-												setEditingField(field);
-												setEditingFieldValue(value);
-											}}
-										>
-											<img src="/svgs/edit.svg" alt="" />
-										</button>
-									</div>
-								);
-							})}
-					</div>
-					<button className="bg-indigo-800 hover:bg-indigo-700 hover:cursor-pointer w-60 h-12 rounded-lg text-lg font-medium" onClick={saveUser}>
-						Save Changes
-					</button>
+	return (
+		<>
+			{mainLoading ? (
+				<MainLoading />
+			) : (
+				<div className="bg-[#160f33] h-full w-full rounded-lg overflow-auto">
+					{loading ? (
+						<Loading />
+					) : (
+						<div className="min-w-100 w-full shrink xl:min-w-full overflow-x-auto p-4 flex flex-col gap-y-4">
+							<div className="grid grid-cols-1 lg:grid-cols-2 gap-y-4 gap-x-2">
+								{user &&
+									Object.entries(user).map(([field, value]) => {
+										return (
+											<div key={field} className="flex justify-between items-center min-w-fit w-full shadow/30 bg-indigo-950/40 rounded-lg hover:bg-indigo-950 border-b border-b-white/5 p-3">
+												<div className="flex flex-col text-lg h-full shrink">
+													<h6 className="text-gray-400 truncate">{field}</h6>
+													{field === editingField ? <input id="editing-value" value={editingFieldValue} onChange={(e) => setEditingFieldValue(e.target.value)} onBlur={() => setEditingField(null)} /> : <span className="truncate">{value}</span>}
+												</div>
+
+												<button
+													className="justify-center items-center flex hover:cursor-pointer bg-indigo-700 rounded-md p-1 w-10 h-10 shrink-0"
+													aria-label="Edit this value"
+													onClick={() => {
+														setEditingField(field);
+														setEditingFieldValue(value);
+													}}
+												>
+													<img src="/svgs/edit.svg" alt="" />
+												</button>
+											</div>
+										);
+									})}
+							</div>
+							<button className="bg-indigo-800 hover:bg-indigo-700 hover:cursor-pointer w-60 h-12 rounded-lg text-lg font-medium" onClick={saveUser}>
+								Save Changes
+							</button>
+							<button className="bg-rose-800 hover:bg-rose-700 hover:cursor-pointer w-60 h-12 rounded-lg text-lg font-medium flex justify-center items-center gap-x-1" onClick={logout}>
+								<img src="/svgs/logout.svg" />
+								<span>Logout</span>
+							</button>
+						</div>
+					)}
 				</div>
 			)}
-		</div>
+		</>
 	);
 }
