22b16e7fbcc0c90933a026838d1f4c6bd56af28e
Brett Boettcher <hgdxg@BrettsRTX>
2025-07-30 19:49:05 -0600

    added loading screen to the profile page

--- client/src/app/canvas/profile/page.js
+++ client/src/app/canvas/profile/page.js
@@ -3,9 +3,12 @@
 import { useState, useEffect } from "react";
 import { useTitleContext } from "../contexts/TitleContext";
 import { useRouter } from "next/navigation";
+import Loading from "@/app/canvas/loading";
+import { setLazyProp } from "next/dist/server/api-utils";
 
 export default function SettingsPage() {
 	const { updateTitle } = useTitleContext();
+	const [loading, setLoading] = useState(true);
 	const [token, setToken] = useState("");
 	const [user, setUser] = useState();
 	const [editingField, setEditingField] = useState();
@@ -20,6 +23,7 @@ export default function SettingsPage() {
 	}, []);
 
 	async function fetchUser() {
+		setLoading(true);
 		let storedToken = token;
 		if (!storedToken) {
 			storedToken = localStorage.getItem("jwtToken");
@@ -35,6 +39,7 @@ export default function SettingsPage() {
 		const { user } = await userResponse.json();
 		setUser(user);
 		console.log(user);
+		setLoading(false);
 	}
 
 	useEffect(() => {
@@ -51,7 +56,8 @@ export default function SettingsPage() {
 		}));
 	}, [editingFieldValue]);
 
-	async function saveUser () {
+	async function saveUser() {
+		setLoading(true);
 		console.log(token);
 		const updateResponse = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/users/self/update`, {
 			method: "PATCH",
@@ -63,39 +69,44 @@ export default function SettingsPage() {
 				user,
 			}),
 		});
+		setLoading(false);
 	}
 
 	return (
 		<div className="bg-[#160f33] h-full w-full rounded-lg overflow-auto">
-			<div className="min-w-100 w-full shrink xl:min-w-full overflow-x-auto p-4 flex flex-col gap-y-4">
-				<div className="grid grid-cols-1 lg:grid-cols-2 gap-y-4 gap-x-2">
-					{user &&
-						Object.entries(user).map(([field, value]) => {
-							return (
-								<div key={field} className="flex justify-between items-center min-w-fit w-full shadow/30 bg-indigo-950/40 rounded-lg hover:bg-indigo-950 border-b border-b-white/5 p-3">
-									<div className="flex flex-col text-lg h-full shrink">
-										<h6 className="text-gray-400 truncate">{field}</h6>
-										{field === editingField ? <input id="editing-value" value={editingFieldValue} onChange={(e) => setEditingFieldValue(e.target.value)} onBlur={() => setEditingField(null)} /> : <span className="truncate">{value}</span>}
-									</div>
+			{loading ? (
+				<Loading />
+			) : (
+				<div className="min-w-100 w-full shrink xl:min-w-full overflow-x-auto p-4 flex flex-col gap-y-4">
+					<div className="grid grid-cols-1 lg:grid-cols-2 gap-y-4 gap-x-2">
+						{user &&
+							Object.entries(user).map(([field, value]) => {
+								return (
+									<div key={field} className="flex justify-between items-center min-w-fit w-full shadow/30 bg-indigo-950/40 rounded-lg hover:bg-indigo-950 border-b border-b-white/5 p-3">
+										<div className="flex flex-col text-lg h-full shrink">
+											<h6 className="text-gray-400 truncate">{field}</h6>
+											{field === editingField ? <input id="editing-value" value={editingFieldValue} onChange={(e) => setEditingFieldValue(e.target.value)} onBlur={() => setEditingField(null)} /> : <span className="truncate">{value}</span>}
+										</div>
 
-									<button
-										className="justify-center items-center flex hover:cursor-pointer bg-indigo-700 rounded-md p-1 w-10 h-10 shrink-0"
-										aria-label="Edit this value"
-										onClick={() => {
-											setEditingField(field);
-											setEditingFieldValue(value);
-										}}
-									>
-										<img src="/svgs/edit.svg" alt="" />
-									</button>
-								</div>
-							);
-						})}
+										<button
+											className="justify-center items-center flex hover:cursor-pointer bg-indigo-700 rounded-md p-1 w-10 h-10 shrink-0"
+											aria-label="Edit this value"
+											onClick={() => {
+												setEditingField(field);
+												setEditingFieldValue(value);
+											}}
+										>
+											<img src="/svgs/edit.svg" alt="" />
+										</button>
+									</div>
+								);
+							})}
+					</div>
+					<button className="bg-indigo-800 hover:bg-indigo-700 hover:cursor-pointer w-60 h-12 rounded-lg text-lg font-medium" onClick={saveUser}>
+						Save Changes
+					</button>
 				</div>
-				<button className="bg-indigo-800 hover:bg-indigo-700 hover:cursor-pointer w-60 h-12 rounded-lg text-lg font-medium" onClick={saveUser}>
-					Save Changes
-				</button>
-			</div>
+			)}
 		</div>
 	);
 }
